694f596261978af7ade5b43eba9dea03dbc61ad0
szsz <showmen15@poczta.onet.pl>
2016-10-28 15:27:33 +0200

    konfiguracja

--- location/location.sh
+++ location/location.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export __dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -jar $__dir/target/location-1.0-SNAPSHOT.jar
+java -jar $__dir/target/location-1.0-SNAPSHOT.jar ./location.conf
 
 
